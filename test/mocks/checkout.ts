f50495198a5daec834e8de353bb0b4ac44b49ce6
BlackAnaboth <BLACKANABOTH@yandex.com>
2023-09-15 23:13:31 -0500

    subida a nuevo repo tras colapso de VSCode

---
+++ test/mocks/checkout.ts
@@ -0,0 +1,20 @@
+export const checkoutMocked = {
+  customer: {
+    name: "Jacobo",
+    lastname: "Gaitan",
+    email: "krakar@email.com",
+    address: {
+      address1: "12 345",
+      address2: "98 7654",
+      city: "Bogota",
+      state: "Cuandinamarca",
+      zipCode: "111111",
+    },
+  },
+  order: {
+    name: "Ant-Man (2003) #4",
+    image:
+      "http://i.annihil.us/u/prod/marvel/i/mg/4/20/4bc697c680890.jpg",
+    price: 50,
+  },
+};
